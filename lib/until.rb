fea31a452689f50ee8ce057ed23010a1b6043875
Donna Amos <donnacamos.developer@gmail.com>
2019-02-15 23:35:37 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -4,12 +4,7 @@ def using_until
    until levitation_force == 10 
    puts "Wingardium Leviosa"
    levitation_force += 1 
- 
- while levitation_force < 10
- puts "Wingardium Leviosa"
- levitation_force += 1
- 
- end
+
  end
 end
 
